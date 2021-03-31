083bd51c435f4ed0aec636d2b9b071ece4f83400
sabrtian <55495023+sabrtian@users.noreply.github.com>
2021-03-30 23:24:54 -0400

    Add files via upload
    jquery exercise

---
+++ exercises/jquery/assets/main.js
@@ -0,0 +1,22 @@
+
+$(function() {
+	let count = 0; 
+
+	$(".button").click(function() {
+		count++;
+    
+    	if(count % 3 == 1) {
+	    	$(".dog1").hide();
+	    	$(".dog2").show();
+	    	$(".dog3").show();
+	    } else if (count % 3 == 2) {
+	    	$(".dog1").show();
+	    	$(".dog2").hide();
+	    	$(".dog3").show();
+	    } else {
+	    	$(".dog1").show();
+	    	$(".dog2").show();
+	    	$(".dog3").hide();
+	    }
+  });
+});
